501ee8fe904866840dd97d0092ac2538e8daaade
VictoriaCarvalho <vix219@icloud.com>
2025-02-16 16:19:29 -0600

    add lab

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,8 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
+  // add this line
+  screen.debug();
+
   expect(screen.queryByText("please pass this test")).toBeInTheDocument();
 });
